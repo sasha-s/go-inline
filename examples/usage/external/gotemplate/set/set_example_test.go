3a0319241a0eb4c2c4cdb6094026d5febe89db16
sasha-s <sasha@scaledinference.com>
2015-06-30 11:06:31 -0700

    add an example of generating a code from an external blueprint (gotemplates.set)

---
+++ examples/usage/external/gotemplate/set/set_example_test.go
@@ -0,0 +1,21 @@
+package set
+
+// To make sure `go get` fetches the blueprint.
+import (
+	"fmt"
+	"sort"
+
+	_ "github.com/ncw/gotemplate/set"
+)
+
+// Generate Set[String]
+//go:generate goinline -package=github.com/ncw/gotemplate/set --target-package-name=set -target-dir=. -w A->string
+
+func ExampleSet() {
+	s := NewSet().Add("gotemplate").Add("goinline").AsList()
+	sort.Strings(s)
+	fmt.Println(s)
+
+	// Output: [goinline gotemplate]
+
+}
